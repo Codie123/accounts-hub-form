92b48c558b46de65534b9a0c4135a40c680bf3f7
Codie@123 <elliott6518@gmail.com>
2023-07-06 15:43:50 +0530

    Final Commite
    Mod:Updated Mailing function ,with attached files.

--- dom-to-pdf.js
+++ dom-to-pdf.js
@@ -1,20 +1,45 @@
-const button = document.querySelector(".submit");
-var element = document.getElementById("test");
-var opt = {
-  margin: 1,
-  filename: "myfile.pdf",
-  image: { type: "jpeg", quality: 0.98 },
+// const PUBLIC_KEY = "FmTw3vk_Mi14SA1sA";
+// (function () {
+//   emailjs.init(PUBLIC_KEY);
+// })();
 
-  jsPDF: { unit: "px", format: [1519.2, 1428.19], orientation: "portrait" },
-};
+// const button = document.querySelector(".submit");
+// var element = document.getElementById("test");
+// var opt = {
+//   margin: 1,
+//   filename: "myfile.pdf",
+//   image: { type: "jpeg", quality: 0.98 },
 
-button.addEventListener("click", () => {
-  button.style.display = "none";
-  html2pdf()
-    .set(opt)
-    .from(element)
-    .save()
-    .then(() => {
-      button.style.display = "block";
-    });
+//   jsPDF: { unit: "px", format: [1519.2, 1428.19], orientation: "portrait" },
+// };
+
+$(document).ready(function () {
+  const node = document.querySelector("#test");
+  $(".submit").click(function () {
+    button.style.display = "none";
+    domtoimage
+      .toPng(node)
+      .then(function (dataUrl) {
+        var img = document.createElement("img");
+        img.src = dataUrl;
+
+        button.style.display = "block";
+        Email.send({
+          SecureToken: "f45d3100-0419-40f5-88b8-fd990ffaa237",
+          To: "ahexam22@gmail.com",
+          From: "elliott6518@gmail.com",
+          Subject: "Accounts Hub Vat Form",
+          Body: "Form Attached below",
+          Attachments: [
+            {
+              name: "image.png",
+              data: dataUrl,
+            },
+          ],
+        }).then((message) => alert(`${message} Mail Sent Successfully`));
+      })
+      .catch(function (error) {
+        console.error("oops, something went wrong!", error);
+      });
+  });
 });
